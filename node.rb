c9ba7b82d63810188f500f7bd3dc772ce4e00b2c
Alan <alanrejnowicz281@gmail.com>
2020-08-31 10:31:15 +0200

    Added Node class to check the properties of the object

---
+++ node.rb
@@ -0,0 +1,45 @@
+class Node 
+  attr_accessor :root, :left, :right
+
+  def initialize(root, left = nil, right = nil) 
+    @root = root 
+    @left = left 
+    @right = right
+  end
+
+  def has_only_one_child?
+    (left != nil && right == nil) || (left == nil && right != nil)
+  end
+
+  def has_no_children?
+    left == nil && right == nil
+  end 
+
+  def has_two_children?
+    left != nil && right != nil 
+  end
+
+  def has_left?
+    left != nil 
+  end 
+
+  def has_only_left? 
+    left != nil && right == nil 
+  end 
+
+  def has_no_left? 
+    left == nil 
+  end 
+
+  def has_right?
+    right != nil 
+  end 
+
+  def has_only_right?
+    right != nil && left == nil
+  end 
+
+  def has_no_right? 
+    right == nil
+  end   
+end
