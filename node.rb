c53900ceec7cc6486a2551467ae18d47b77bb371
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2020-12-06 23:40:43 +0100

    Changed name of :root to :data, deleted unecessary methods, updated code

--- node.rb
+++ node.rb
@@ -1,55 +1,33 @@
 class Node 
-  attr_accessor :root, :left, :right
+    attr_accessor :data, :left, :right 
 
-  def initialize(root, left = nil, right = nil) 
-    @root = root 
-    @left = left 
-    @right = right
-  end
-
-  def has_only_one_child?
-    (left != nil && right == nil) || (left == nil && right != nil)
-  end
-
-  def has_no_children?
-    left == nil && right == nil
-  end 
-
-  def has_two_children?
-    left != nil && right != nil 
-  end
-
-  def has_left?
-    left != nil 
-  end 
-
-  def has_only_left? 
-    left != nil && right == nil 
-  end 
-
-  def has_no_left? 
-    left == nil 
-  end 
-
-  def has_right?
-    right != nil 
-  end 
-
-  def has_only_right?
-    right != nil && left == nil
-  end 
-
-  def has_no_right? 
-    right == nil
-  end   
-
-  def only_child 
-    if has_only_one_child? 
-      if has_left? 
-        left 
-      elsif has_right? 
-        right
-      end 
+    def initialize(data,left = nil,right = nil)
+        @data = data
+        @left = left 
+        @right = right
     end 
-  end 
-end
+
+    def is_leaf_node?
+        left == nil && right == nil
+    end 
+
+    def has_only_right?
+        left == nil && right != nil
+    end 
+
+    def has_only_left?
+        left != nil && right == nil 
+    end 
+
+    def only_child 
+        has_only_right? ? right : has_only_left? ? left : return
+    end 
+
+    def has_left?
+        left != nil
+    end 
+
+    def has_right? 
+        right != nil
+    end 
+end     
